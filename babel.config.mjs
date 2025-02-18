0c17dc31a3858bc84df9438968f732e08650fd33
Pranav Bajaj <pb555544g@gmail.com>
2025-02-18 12:05:18 +0530

    Added Reporting using JUnit Reporter - 18/02/25

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,4 +1,11 @@
 export default {
+    reporters: [
+      "default",
+      ["jest-junit", { outputDirectory: "test-results", outputName: "jest-report.xml" }]
+    ],
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageReporters: ["json", "lcov", "text", "clover"],
     presets: [
       '@babel/preset-env'
     ],
