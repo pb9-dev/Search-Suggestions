3d89c5707aca4a80cf8f8f03299030d0b397f9aa
Pranav Bajaj <pb555544g@gmail.com>
2025-02-27 11:18:03 +0530

    Added addition of sql server and scripts to CI/CD pipeline

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -e  # Exit if any command fails
+set -e  # Exit if any command fail
 
 echo "Updating packages..."
 sudo apt update -y && sudo apt upgrade -y
 
-echo "Installing AWS CodeDeploy Agent.."
+
 sudo apt install ruby-full -y
 wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install -O codedeploy-install.sh
 chmod +x codedeploy-install.sh
@@ -12,33 +12,31 @@ sudo ./codedeploy-install.sh auto
 sudo systemctl enable codedeploy-agent
 sudo systemctl start codedeploy-agent
 
-echo "Installing .NET 9."
+
 wget -q https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt update -y
 sudo apt install -y dotnet-sdk-9.0
 
-echo "Installing Redis.."
 sudo apt install -y redis-server
 sudo systemctl enable redis-server
 sudo systemctl start redis-server
 
-echo "Installing SQL Server..."
+
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
 sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list)"
 sudo apt update -y
 sudo apt install -y mssql-server
 
-echo "Configuring SQL Server.."
-sudo MSSQL_SA_PASSWORD='YourStrong!Passw0rd' MSSQL_PID='Express' /opt/mssql/bin/mssql-conf setup accept-eula
 
-echo "Installing Microsoft SQL Server tools..."
+sudo MSSQL_SA_PASSWORD='abc' MSSQL_PID='Express' /opt/mssql/bin/mssql-conf setup accept-eula
+
+
 curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
 sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list)"
 sudo apt update
 sudo apt install -y mssql-tools unixodbc-dev
 
-# Add sqlcmd to PATH for immediate use
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 
@@ -46,6 +44,6 @@ echo "Waiting for SQL Server to start..."
 sleep 30  # Wait to ensure SQL Server is running
 
 echo "Executing SQL script..."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'YourStrong!Passw0rd' -d master -i /var/www/backend/scripts/script.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'abc' -d master -i /var/www/backend/scripts/script.sql
 
 echo "Completed."
